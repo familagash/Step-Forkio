f1c87eddd18e2277ded96717b0fb7a999a19524c
emirrahim <rahimmr011@gmail.com>
2019-12-05 08:41:50 -0800

    init

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
-/*
-const hNavRow = document.querySelector(".header__nav-row");
 
-if(screen.availWidth > 992)
-{
-    hNavRow.remove();
-}
-*/
+const hamburger = document.querySelector(".header__nav-hamburger");
+const hMenu = document.querySelector(".header__menu");
+const hMenuItem = document.querySelectorAll(".header__menu-link");
+
+hamburger.addEventListener("click" , (e)=>{
+    if(hMenu.classList.contains("active-drop")){
+        hMenu.classList.remove("active-drop");
+        hMenu.style.display = "none";
+    }else {
+        hMenu.classList.add("active-drop");
+        hMenu.style.display = "flex";
+    }
+});
+
